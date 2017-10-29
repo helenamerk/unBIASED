9d47221bb85d49edece162ea479ad90933daf566
Pedro Morais <autranemorais@gmail.com>
2017-10-28 20:00:17 -0700

    added server

---
+++ server/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+
+const app = express();
+
+app.get('/allsides/:query', function(req, res) {
+    var query = req.params.query
+    console.log("query: " + query + "\n")
+    var API_KEY = "8317b7ec4aff48269c4fe72bff5c05a0"
+    request({
+        uri: "https://www.allsides.com/bias/bias-ratings",
+
+    }, function(error, response, body) {
+        var $ = cheerio.load(body);
+        table = $('table > tbody > tr > td > a');
+
+        outlet_to_bias = {}
+
+        for(var i = 0; i < table.length - 1; i+=2) {
+            outlet_str = table[i]["attribs"]["href"];
+            bias_str = table[i+1]["attribs"]["href"];
+
+            outlet = outlet_str.slice(13).split("-").join(" ").replace(' bias', '');
+            bias = bias_str.slice(12).split("-").join(" ");
+
+            outlet_to_bias[outlet] = bias;
+            //console.log(outlet + ": " + bias)
+        }
+
+        request({
+            uri: "https://api.cognitive.microsoft.com/bing/v7.0/news/search?q=" + query + "&headlineCount=20",
+            method: "GET",
+            headers: {
+                'Ocp-Apim-Subscription-Key': "49b4efad8e8249f783f37e262876276e",
+            } 
+        }, function(error, response, body) {
+            news = JSON.parse(body);
+            values = news["value"]
+
+            for(var i = 0; i < values.length; i++) {
+                outlet = values[i]["provider"][0]['name'].toLowerCase().replace('the ', '');
+                title = values[i]["name"];
+
+                if(outlet in outlet_to_bias) {
+                    console.log("Title: " + title)
+                    console.log("Outlet: " + outlet)
+                    console.log("Bias: " + outlet_to_bias[outlet])
+                    console.log("")
+                }
+                
+
+            }
+            res.send(body);
+        });
+    });
+});
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000');
+})
